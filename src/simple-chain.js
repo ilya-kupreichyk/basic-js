b423daef5bc635eb105d74d219ac35fd0efa3100
ilya-kupreichyk <kupreichykilya@gmail.com>
2020-11-01 17:41:39 +0300

    unit: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
+let chain = [];
+
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chain.push(value === null || typeof value === "undefined" ? "null" : value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    position -= 1;
+    if (
+      !(
+        Number.isInteger(position) &&
+        position >= 0 &&
+        position < this.getLength() - 1
+      )
+    ) {
+      chain = [];
+      throw new Error();
+    }
+    chain.splice(position, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let chainView = `( ${chain.join(" )~~( ")} )`;
+    chain = [];
+    return chainView;
+  },
 };
 
 module.exports = chainMaker;
